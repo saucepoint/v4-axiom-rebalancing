e90d3cf6d9d4f8abef5db3d99a74b535bfdd0554
saucepoint <98790946+saucepoint@users.noreply.github.com>
2023-09-23 11:06:11 -0400

    Initial axiom integration (#1)
    * initial axiom integration
    
    * fork v4-core position modifier
    
    * ignore vscode
    
    * forge fmt
    
    * guess work on the storage slot of spot price data
    
    * actual bottom 24 bits

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import {BaseHook} from "v4-periphery/BaseHook.sol";
 
 import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {Pool} from "@uniswap/v4-core/contracts/libraries/Pool.sol";
 import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
 import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
@@ -44,7 +45,7 @@ contract Counter is BaseHook {
 
     function beforeModifyPosition(
         address,
-        PoolKey calldata,
+        PoolKey calldata key,
         IPoolManager.ModifyPositionParams calldata,
         bytes calldata hookData
     ) external override returns (bytes4) {
@@ -59,6 +60,32 @@ contract Counter is BaseHook {
             response.storageResponses
         );
         if (!valid) revert("invalid axiom");
+
+        IAxiomV1Query.StorageResponse memory storageData = response.storageResponses[0];
+
+        // state data is coming from PoolManager
+        require(storageData.addr == address(poolManager), "axiom: not poolmanager");
+
+        // TODO: state data is more than 24 hours old
+        require(storageData.blockNumber < block.number, "axiom: state too new");
+
+        // TODO: confirm its slot0 holds tick data
+        require(storageData.slot == 0, "axiom: not slot0");
+
+        // get old tick and current tick
+        // assuming we use `cast index bytes32 <poolId> 10`, we get a storage slot for Pool.State
+        // luckily slot0 is the first 216 bits of Pool.State
+        uint256 value = storageData.value;
+        int24 oldTick;
+        assembly {
+            oldTick := shr(32, value) // clear out the bottom 32 bits, fee data
+            // keep the bottom 24 bits
+            oldTick := and(0xFFFFFF, oldTick)
+        }
+        (, int24 tick,,,,) = poolManager.getSlot0(key.toId());
+
+        // TODO: old state meets market conditions
+        require(oldTick < tick, "axiom: market conditions not met");
 
         return BaseHook.beforeModifyPosition.selector;
     }
