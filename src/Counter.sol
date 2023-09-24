7445b8bbbb96eb852c96df404bd7710b2723cbac
saucepoint <98790946+saucepoint@users.noreply.github.com>
2023-09-24 01:02:32 -0400

    axiom v1 to v2 (#5)
    Co-authored-by: Andrew Forman <andrewm.forman@gmail.com>

--- src/Counter.sol
+++ src/Counter.sol
@@ -9,24 +9,12 @@ import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.s
 import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
 import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
-import {IAxiom} from "./external/interfaces/IAxiomV1Query.sol";
+import {IAxiomV2client} from "./external/interfaces/IAxiomV2client.sol";
 
-struct ResponseStruct {
-    bytes32 keccakBlockResponse;
-    bytes32 keccakAccountResponse;
-    bytes32 keccakStorageResponse;
-    IAxiomV1Query.BlockResponse[] blockResponses;
-    IAxiomV1Query.AccountResponse[] accountResponses;
-    IAxiomV1Query.StorageResponse[] storageResponses;
-}
-
-contract Counter is BaseHook,  {
+contract Counter is BaseHook, AxiomV2Client {
     using PoolIdLibrary for PoolKey;
 
     uint256 public afterModifyPositionCount;
-
-    // TODO: immutable / set on deployment?
-    IAxiomV1Query public constant axiomQuery = IAxiomV1Query(0x7DFbaa7a8E8f6e9b86C8EbDE4B7bd1E6bFf8Fae6);
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -43,13 +31,19 @@ contract Counter is BaseHook,  {
         });
     }
 
+    // axiom returns blockNumber, pool address, and price
+    // strategy returns blockc
+
     function beforeModifyPosition(
         address,
         PoolKey calldata key,
         IPoolManager.ModifyPositionParams calldata,
         bytes calldata hookData
     ) external override returns (bytes4) {
-        (address sender, bytes memory data) = abi.decode(hookData, (address, bytes));
+        (address sender, bytes memory data) = abi.decode(
+            hookData,
+            (address, bytes)
+        );
         address owner = abi.decode(data, (address));
         require(sender == owner, "axiom: not owner");
 
@@ -66,44 +60,5 @@ contract Counter is BaseHook,  {
     ) external override returns (bytes4) {
         afterModifyPositionCount++;
         return BaseHook.afterModifyPosition.selector;
-    }
-
-    function _handleAxiom(PoolKey memory key, bytes calldata hookData) internal view {
-        ResponseStruct memory response = abi.decode(hookData, (ResponseStruct));
-        bool valid = axiomQuery.areResponsesValid(
-            response.keccakBlockResponse,
-            response.keccakAccountResponse,
-            response.keccakStorageResponse,
-            response.blockResponses,
-            response.accountResponses,
-            response.storageResponses
-        );
-        if (!valid) revert("invalid axiom");
-
-        IAxiomV1Query.StorageResponse memory storageData = response.storageResponses[0];
-
-        // state data is coming from PoolManager
-        require(storageData.addr == address(poolManager), "axiom: not poolmanager");
-
-        // TODO: state data is more than 24 hours old
-        require(storageData.blockNumber < block.number, "axiom: state too new");
-
-        // TODO: confirm its slot0 holds tick data
-        require(storageData.slot == 0, "axiom: not slot0");
-
-        // get old tick and current tick
-        // assuming we use `cast index bytes32 <poolId> 10`, we get a storage slot for Pool.State
-        // luckily slot0 is the first 216 bits of Pool.State
-        uint256 value = storageData.value;
-        int24 oldTick;
-        assembly {
-            oldTick := shr(32, value) // clear out the bottom 32 bits, fee data
-            // keep the bottom 24 bits
-            oldTick := and(0xFFFFFF, oldTick)
-        }
-        (, int24 tick,,,,) = poolManager.getSlot0(key.toId());
-
-        // TODO: old state meets market conditions
-        require(oldTick < tick, "axiom: market conditions not met");
     }
 }
